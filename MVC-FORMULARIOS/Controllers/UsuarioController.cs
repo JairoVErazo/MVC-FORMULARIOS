f0d0d2b6154c4906998ce19ebc2e7217d8287dc1
Jairo Erazo <132089241+JairoVErazo@users.noreply.github.com>
2024-03-23 12:45:47 -0600

    Agregar archivos de proyecto.

---
+++ MVC-FORMULARIOS/Controllers/UsuarioController.cs
@@ -0,0 +1,37 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using MVC_FORMULARIOS.Models;
+
+namespace MVC_FORMULARIOS.Controllers
+{
+    public class UsuarioController : Controller
+    {
+        private readonly MvcContext _context;
+
+        public UsuarioController( MvcContext context)
+        {
+            _context = context;
+        }
+
+
+        public IActionResult Crear()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> Crear(Usuario usuario)
+        {
+
+            if (ModelState.IsValid)
+            {
+                _context.Add(usuario);
+                _context.SaveChanges();
+                return RedirectToAction("Index", "Home");
+            }
+
+            return View(usuario);
+
+        }
+
+    }
+}
---
+++ MVC-FORMULARIOS/Program.cs
@@ -0,0 +1,41 @@
+using Microsoft.EntityFrameworkCore;
+using MVC_FORMULARIOS.Models;
+
+namespace MVC_FORMULARIOS
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            var builder = WebApplication.CreateBuilder(args);
+
+            // Add services to the container.
+            builder.Services.AddControllersWithViews();
+            builder.Services.AddDbContext<MvcContext>(opciones => 
+            opciones.UseSqlServer("name=DefaultConnection"));
+
+            var app = builder.Build();
+
+            // Configure the HTTP request pipeline.
+            if (!app.Environment.IsDevelopment())
+            {
+                app.UseExceptionHandler("/Home/Error");
+                // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
+                app.UseHsts();
+            }
+
+            app.UseHttpsRedirection();
+            app.UseStaticFiles();
+
+            app.UseRouting();
+
+            app.UseAuthorization();
+
+            app.MapControllerRoute(
+                name: "default",
+                pattern: "{controller=Usuario}/{action=Crear}/{id?}");
+
+            app.Run();
+        }
+    }
+}
